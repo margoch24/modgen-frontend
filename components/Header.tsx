49df8bfee2d3631017509fc74aa8599239248364
Margo <vitarita095788@gmail.com>
2025-02-28 01:15:57 +0200

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div
+      className="bg-black min-h-[150px] pl-20 flex items-center"
+      style={{ borderBottomLeftRadius: "60px" }}
+    >
+      <div>
+        <h1 className="text-3xl text-white">ModGen</h1>
+        <h5 className="text-sm text-white">
+          Reversible Modification Image Generator
+        </h5>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
